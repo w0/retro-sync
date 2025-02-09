5f0518a18b50d58e06e1e17104dba0eea7024c99
w0 <33212583+w0@users.noreply.github.com>
2025-02-09 14:57:06 -0600

    use env variables, create logger

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,30 +2,61 @@ package main
 
 import (
 	"database/sql"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/w0/retro-sync/internal/database"
 	_ "modernc.org/sqlite"
 )
 
 type application struct {
-	db *database.Queries
+	logger   *slog.Logger
+	db       *database.Queries
+	rootPath string
 }
 
 func main() {
-	db, err := sql.Open("sqlite", "./retrosync.db?_pragma=foreign_keys(1)")
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		logger.Info(err.Error())
+	}
+
+	rootPath := os.Getenv("ROOT_PATH")
+	if rootPath == "" {
+		logger.Error("ROOT_PATH environment variable not set.")
+		os.Exit(1)
+	}
+
+	dbUrl := os.Getenv("DATABASE_URL")
+	if dbUrl == "" {
+		logger.Error("DATABASE_URL evironment variable not set.")
+	}
+
+	db, err := sql.Open("sqlite", dbUrl)
 
 	if err != nil {
-		log.Fatalf("failed to open db: %s", err)
+		logger.Error(err.Error())
+		os.Exit(1)
 	}
 
 	defer db.Close()
 
 	app := application{
-		db: database.New(db),
+		db:       database.New(db),
+		logger:   logger,
+		rootPath: rootPath,
 	}
 
-	log.Println("starting server on :4000")
-	http.ListenAndServe(":4000", app.routes())
+	port := os.Getenv("PORT")
+	if port == "" {
+		app.logger.Warn("PORT environment variable unset. Default to 8080")
+		port = "8080"
+	}
+
+	app.logger.Info("starting server", "PORT", port)
+	http.ListenAndServe(":"+port, app.routes())
 }
